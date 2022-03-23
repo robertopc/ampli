7a40c36745677857f311b9afdf0b2f5d7417ecab
robertopc <robertopc.prog@gmail.com>
2022-03-23 17:31:28 -0300

    Moved unidade1 files; Updated compilar.py

--- compilar.py
+++ compilar.py
@@ -7,25 +7,29 @@ Compile the C files of the aulas of the given number
 import subprocess
 import sys
 import os
-
-number = 0
+from unicodedata import unidata_version
 
 if len(sys.argv) < 2:
     # check if was passed argument
-    print("usage: ", sys.argv[0], " <numero-aula>")
+    print("usage: ", sys.argv[0], " <aula> [unidade]")
     sys.exit()
 elif not sys.argv[1].isdigit():
     # check if is digit
-    print("usage: ", sys.argv[0], " <numero-aula2>")
+    print("usage: ", sys.argv[0], " <aula> [unidade]")
     sys.exit()
 
-number = sys.argv[1]
+aula = sys.argv[1]
+try:
+    unidade = sys.argv[2] if sys.argv[2].isdigit() else 1
+except IndexError:
+    unidade = 1
 
+os.chdir('unidade'+str(unidade))
 dirlist = os.listdir()
 
 # search for the files to compile
 for file in dirlist:
-    if file.find('aula'+number.zfill(2)) > -1 and file.find('.c') > -1:
+    if file.find('aula'+aula.zfill(2)) > -1 and file.find('.c') > -1:
         filepath = os.path.abspath(file)
         print("Compiling "+file+"...")
         # send the compiler command to shell
